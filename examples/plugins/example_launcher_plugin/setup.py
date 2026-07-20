d8b5aa8f933cfa899c0fcb9bf08e265d67222bfa
vitorguzman116-sketch <vitorguzman116@gmail.com>
2026-07-19 23:24:01 -0600

    Initialize pr3251

---
+++ examples/plugins/example_launcher_plugin/setup.py
@@ -0,0 +1,39 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# type: ignore
+from setuptools import find_namespace_packages, setup
+
+with open("README.md") as fh:
+    LONG_DESC = fh.read()
+    setup(
+        name="hydra-example-launcher",
+        version="1.0.0",
+        author="Omry Yadan",
+        author_email="omry@fb.com",
+        description="Example Hydra Launcher plugin",
+        long_description=LONG_DESC,
+        long_description_content_type="text/markdown",
+        url="https://github.com/facebookresearch/hydra/",
+        license="MIT",
+        packages=find_namespace_packages(include=["hydra_plugins.*"]),
+        classifiers=[
+            # Hydra uses Python version and Operating system to determine
+            # In which environments to test this plugin
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
+            "Programming Language :: Python :: 3.14",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.10",
+        install_requires=[
+            # consider pinning to a specific major version of Hydra to avoid unexpected problems
+            # if a new major version of Hydra introduces breaking changes for plugins.
+            # e.g: "hydra-core==1.0.*",
+            "hydra-core",
+        ],
+        # If this plugin is providing configuration files, be sure to include them in the package.
+        # See MANIFEST.in.
+        # For configurations to be discoverable at runtime, they should also be added to the search path.
+        include_package_data=True,
+    )
---
+++ hydra/plugins/plugin.py
@@ -0,0 +1,5 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from abc import ABC
+
+
+class Plugin(ABC): ...
