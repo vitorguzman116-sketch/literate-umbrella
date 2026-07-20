98510e8c08de441b843259beacc90d959d4ab0fa
vitorguzman116-sketch <vitorguzman116@gmail.com>
2026-07-19 23:23:55 -0600

    Initialize pr3244

---
+++ setup.py
@@ -0,0 +1,62 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# type: ignore
+import pathlib
+
+from setuptools import find_namespace_packages, setup
+
+from build_helpers.build_helpers import (
+    ANTLRCommand,
+    BuildPyCommand,
+    CleanCommand,
+    Develop,
+    SDistCommand,
+    find_version,
+)
+
+with pathlib.Path("requirements/requirements.txt").open() as requirements_txt:
+    install_requires = [
+        stripped
+        for line in requirements_txt
+        if (stripped := line.strip()) and not stripped.startswith("#")
+    ]
+
+
+with open("README.md") as fh:
+    LONG_DESC = fh.read()
+    setup(
+        cmdclass={
+            "antlr": ANTLRCommand,
+            "clean": CleanCommand,
+            "sdist": SDistCommand,
+            "build_py": BuildPyCommand,
+            "develop": Develop,
+        },
+        name="hydra-core",
+        version=find_version("hydra", "__init__.py"),
+        author="Omry Yadan",
+        author_email="omry@fb.com",
+        description="A framework for elegantly configuring complex applications",
+        license="MIT",
+        long_description=LONG_DESC,
+        long_description_content_type="text/markdown",
+        url="https://github.com/facebookresearch/hydra",
+        keywords="command-line configuration yaml tab-completion",
+        packages=find_namespace_packages(include=["hydra", "hydra.*"]),
+        include_package_data=True,
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
+            "Programming Language :: Python :: 3.14",
+            "Operating System :: POSIX :: Linux",
+            "Operating System :: MacOS",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        python_requires=">=3.10",
+        install_requires=install_requires,
+        entry_points={"pytest11": ["hydra_pytest = hydra.extra.pytest_plugin"]},
+        # Install development dependencies with
+        # pip install -r requirements/dev.txt -e .
+    )
---
+++ website/docs/development/style_guide.md
@@ -0,0 +1,29 @@
+---
+id: style_guide
+title: Style Guide
+sidebar_label: Style Guide
+---
+
+The code need to pass verification by the following tools:
+ - `black .` : Automatic code formatting for Python
+ - `flake8` : PEP8 compliance checker for Python, this includes copyright header verification
+ - `isort .` : Ensure imports are sorted properly
+ - `pyrefly check` : Ensures code passes static type checking
+ - `yamllint .` : Ensures that yaml files are syntactically correct and properly indented.
+
+The easiest way to run the required verifications is:
+ - `nox -s lint` : for everything
+ - `nox -s lint-core` : for the Hydra core
+ - `nox -s lint-plugins` : for the included plugins
+
+isort is a bit tricky to run for plugins. the best way to get it to sort the plugins imports is with the FIX environment
+variable:
+```
+$ FIX=1 nox -s lint-plugins
+```
+
+It is also recommended that you install pre-commit hooks (use `pre-commit install`).
+pre-commit will execute some of the above tests when you commit your code locally. 
+You can disable it by appending `-n` to your commit command: `git commit -m wip -n`
+
+Pull requests that do not lint will fail the automated testing.
