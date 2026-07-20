5ec3d0fbb34dfb8292db9e9963ed839ef9096140
vitorguzman116-sketch <vitorguzman116@gmail.com>
2026-07-19 23:23:46 -0600

    Initialize pr3239

---
+++ plugins/hydra_joblib_launcher/setup.py
@@ -0,0 +1,35 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# type: ignore
+from pathlib import Path
+
+from read_version import read_version
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="hydra-joblib-launcher",
+    version=read_version("hydra_plugins/hydra_joblib_launcher", "__init__.py"),
+    author="Jan-Matthis Lueckmann, Omry Yadan",
+    author_email="mail@jan-matthis.de, omry@fb.com",
+    description="Joblib Launcher for Hydra apps",
+    license="MIT",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/facebookresearch/hydra/",
+    packages=find_namespace_packages(include=["hydra_plugins.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "hydra-core>=1.1.0.dev7",
+        "joblib>=0.14.0",
+    ],
+    include_package_data=True,
+)
