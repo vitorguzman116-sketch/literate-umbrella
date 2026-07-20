1c2bd170ebc28c25d6c3c3380bbcdc46d1f1cfee
vitorguzman116-sketch <vitorguzman116@gmail.com>
2026-07-19 23:23:17 -0600

    Initialize pr3205

---
+++ examples/tutorials/basic/running_your_hydra_app/3_working_directory/my_app.py
@@ -0,0 +1,17 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import os
+
+from omegaconf import DictConfig
+
+import hydra
+from hydra.core.hydra_config import HydraConfig
+
+
+@hydra.main(version_base=None)
+def my_app(_cfg: DictConfig) -> None:
+    print(f"Working directory : {os.getcwd()}")
+    print(f"Output directory  : {HydraConfig.get().runtime.output_dir}")
+
+
+if __name__ == "__main__":
+    my_app()
